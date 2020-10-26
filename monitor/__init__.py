e93bb390df88a0b011123dbfb18e2d5435a03357
Fu Hanxi <hfudev@gmail.com>
2020-10-26 23:47:43 +0800

    chore: docker compose and some init configs

---
+++ monitor/__init__.py
@@ -0,0 +1,30 @@
+# Copyright 2020 hanxifu
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pprint import pprint
+
+import itchat
+from itchat.content import TEXT
+
+
+@itchat.msg_register([TEXT])
+def text_reply(msg):
+    pprint(msg['Text'])
+    pprint(msg)
+    pprint(msg.fromUserName)
+
+
+if __name__ == '__main__':
+    itchat.auto_login(hotReload=True)
+    itchat.run(True)
